230c81b7a1e6631d16f79af0db5a76c282596e6f
Marcus Whybrow <marcus@marcuswhybrow.net>
2011-05-26 23:07:10 +0100

    Add the beginnings of the vault page, an example of CSS3 power!

--- littleblog/urls.py
+++ littleblog/urls.py
@@ -18,6 +18,9 @@ urlpatterns = patterns('',
         name='portfolio'),
     url(r'^cv/$', TemplateView.as_view(template_name='cv.html'), name='cv'),
 
+    url(r'^vault/$', TemplateView.as_view(template_name='vault/index.html'),
+        name='vault'),
+
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
